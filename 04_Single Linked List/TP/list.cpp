ea50aab307eb9c2f195532e7bc3b4c0040cffacf
NekoDesu-cyte <haikalfadhilah34@gmail.com>
2024-10-19 22:37:56 +0700

    revisi kode list cpp dan list header

--- 04_Single Linked List/TP/list.cpp
+++ 04_Single Linked List/TP/list.cpp
@@ -18,6 +18,69 @@ void insertFirst(List &L, address P){
     first(L) = P;
 }
 
+void insertLast(List &L, address P) {
+    if (first(L) == NULL) {
+        first(L) = P;
+    } else {
+        address last = first (L);
+        while (next(last) != NULL) {
+            last = next(last);
+        }
+        next(last) = P;
+    }
+}
+
+void insertAfter(List &L, address P, address Prec) {
+    if (Prec != NULL) {
+        next(P) = next(Prec);
+        next(Prec) = P;
+    }
+}
+
+void deleteFirst(List &L) {
+    if (first(L) != NULL) {
+        address P = first(L);
+        first(L) = next(P);
+        delete P;
+    }
+}
+
+void deleteLast(List &L) {
+    if (first(L) != NULL) {
+        address P = first(L);
+        if(next(P) == NULL) {
+            first(L) = NULL;
+        } else {
+            address q;
+            while (next(P) != NULL) {
+                q = P;
+                P = next(P);
+            }
+            next(q) = NULL;
+        }
+        delete P;
+    }
+}
+
+void deleteAfter(List &L, address Prec) {
+    if (Prec != NULL && next(Prec) != NULL) {
+        address p = next(Prec);
+        next(Prec) = next(p);
+        delete p;
+    }
+}
+
+address searchInfo(List L, infotype x) {
+    address p = first(L);
+    while (p != NULL) {
+        if (info(p) == x) {
+            return p;
+        }
+        p = next(p);
+    }
+    return NULL;
+}
+
 void printInfo(List L){
 
     address p = first(L);
